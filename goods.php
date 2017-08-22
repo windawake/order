13a4c16617f72413f1367d4208b33f8f32736337
zhangzibin <zhangzibin@jrq.com>
2017-08-22 10:44:04 +0800

    商品信息

---
+++ goods.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangzibin
+ * Date: 2017/8/22
+ * Time: 10:22
+ */
+
+$goodsList = array(
+    array('id' => 1, 'title' => 'tooth brush'),
+    array('id' => 2, 'title' => 'beer'),
+);
